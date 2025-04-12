6cd2bf934a412cb00e648f14270cd69a7bb1558b
Kate Efimova <kate.efimova0@gmail.com>
2025-04-12 14:42:52 -0700

    removing spec to cut scope, will rely on qa

--- spec/controllers/appointements_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe AppointementsController, type: :controller do
-  context "test" do
-    it "displays all appointements" do
-      get :index
-    end
-  end
-end
--- test/controllers/appointement_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe AppointementController, type: :controller do
-  context "test" do
-    it "displays all appointements" do
-      get :index
-    end
-  end
-end
